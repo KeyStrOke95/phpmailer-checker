fca6aeb9673a4004e4f7aa52bfb32020ca1d8f5a
jasonbernier <jason.bernier@gmail.com>
2017-01-04 10:18:45 -0500

    Update phpmailer_checker.py
    Added some small tweaks to append the VERSION to the url_input and cleaned up the code.

--- phpmailer_checker.py
+++ phpmailer_checker.py
@@ -8,17 +8,28 @@
 import urllib2
 import re
 
-print "PHPMailer Checker"
-print "By Jason Bernier" + '\n'
-
-url_input = raw_input("Enter your URL to test for PHP mailer - EX: http://www.yoursite.com/VERSION " +'\n')
-webtext = urllib2.urlopen(url_input).read()
-matches = re.findall('5.2.16|5.2.17|5.2.18', webtext);
-
-if len(matches) == 0:
-   print url_input + ' DOES NOT appear to have PHPMailer Installed'
-else:
-   print url_input + ' appears to have PHPMailer installed!'
-   print 'The version reported as being installed is '
-   print matches
-   print 'Check https://www.exploit-db.com/exploits/40969/ or https://www.exploit-db.com/exploits/40974/ for a possible exploit'
+def checker(url_input):
+   webtext = urllib2.urlopen(url_input).read()
+   if bool(re.search(r'5\.2\.(?:0|1)[0-8]', webtext)):
+      print url_input + ' appears to have PHPMailer installed!'
+      print 'The version reported as being installed is '
+      print webtext
+      print 'Check https://www.exploit-db.com/exploits/40969/ or https://www.exploit-db.com/exploits/40974/ for a possible exploit'
+   else:
+      print "This version DOES NOT appear to be vulnerable. The current version installed is " + webtext
+      
+def main():
+   print "PHPMailer Checker"
+   print "By Jason Bernier" + '\n'
+   
+   while True:
+      url_input = raw_input("Enter your URL to test for PHP mailer - EX: http://www.yoursite.com\n")
+      if 'http://' in url_input[:7] or 'https://' in url_input[:8]:
+         break
+   if bool(re.search(r'/VERSION$', url_input)):
+      checker(url_input)
+   else: 
+      checker(url_input + "/VERSION")
+        
+if __name__ == "__main__":
+   main()
